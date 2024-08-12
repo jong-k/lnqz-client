ae8a9a93e5b1dc435951ceaf3bafb71761893b39
jong-k <fully.charged07@gmail.com>
2024-08-12 23:20:08 +0900

    feat: footer 저작권 문구 수정

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="h-footer w-full bg-slate-900 font-semibold text-white">
       <div className="container-box flex h-full w-full items-center justify-between">
-        <h2 className="text-xl text-slate-500">© 김종한</h2>
+        <h2 className="text-xl text-slate-500">
+          © {currentYear} 김종한. All rights reserved.
+        </h2>
         <ul className="flex gap-4">
           <li>
             <Link href="/">깃허브</Link>
