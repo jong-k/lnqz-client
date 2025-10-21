78a15bc38f9bb5ec2b15bf228ebac5a3b3a73fd9
jong-k <fully.charged07@gmail.com>
2025-10-21 15:20:50 +0900

    chore: eslint setup

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import { importX } from "eslint-plugin-import-x";
+import pluginPerfectionist from "eslint-plugin-perfectionist";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import { configs as sonarConfigs } from "eslint-plugin-sonarjs";
+import pluginUnicorn from "eslint-plugin-unicorn";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import { configs as tsConfigs } from "typescript-eslint";
+import js from "@eslint/js";
+import pluginNext from "@next/eslint-plugin-next";
+
+export default defineConfig([
+  { ignores: [".next/**"] },
+  {
+    extends: ["js/recommended"],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+    plugins: { js },
+  },
+
+  importX.flatConfigs.recommended,
+  importX.flatConfigs.typescript,
+  ...tsConfigs.recommended,
+  pluginNext.flatConfig.coreWebVitals,
+  pluginUnicorn.configs.recommended,
+  pluginPerfectionist.configs["recommended-natural"],
+  {
+    files: ["**/*.{js,mjs,cjs,ts,tsx,mts,cts}"],
+    rules: {
+      "@typescript-eslint/triple-slash-reference": "off",
+      "import-x/no-named-as-default-member": "off",
+      "perfectionist/sort-imports": "off",
+      "unicorn/prevent-abbreviations": "off",
+    },
+  },
+  sonarConfigs.recommended,
+  eslintPluginPrettierRecommended,
+]);
