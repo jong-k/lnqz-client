882c7fed02629f25e4c12ac7303c80a8968a6cff
jong-k <fully.charged07@gmail.com>
2025-10-21 17:28:58 +0900

    feat: add TopBanner components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import TopBanner from "./_components/top-banner";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold">안녕하세요</h1>;
+  return (
+    <div>
+      <TopBanner text="🎉 Link Squeeze 서비스 오픈 🎉" url="/" />
+      <h1 className="text-3xl font-bold">안녕하세요</h1>
+    </div>
+  );
 }
