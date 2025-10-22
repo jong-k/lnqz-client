4e6c663687dcbc258374eb065ede25e72846de4e
jong-k <fully.charged07@gmail.com>
2025-10-22 18:05:47 +0900

    docs: add comment

--- src/app/_components/footer/index.tsx
+++ src/app/_components/footer/index.tsx
@@ -6,6 +6,7 @@ import FooterMenu from "./footer-menu";
 export default function Footer() {
   return (
     <footer>
+      {/* footer element 없애면 쪼그라듬 */}
       <MaxWidthWrapper>
         <FooterMenu />
         <Divider />
