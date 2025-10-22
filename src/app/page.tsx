bb1460c1057c670da9e0a05b238e165db9dd3d3b
jong-k <fully.charged07@gmail.com>
2025-10-22 16:35:32 +0900

    chore: remove empty classname

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Shortener from "./_components/shortener";
 export default function Home() {
   return (
     <div className="w-full bg-slate-100 py-20">
-      <MaxWidthWrapper className="">
+      <MaxWidthWrapper>
         <Shortener />
       </MaxWidthWrapper>
     </div>
