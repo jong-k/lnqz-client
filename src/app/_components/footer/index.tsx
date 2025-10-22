346d83a8b7940090cc8145ce9318eb77cc37c4ec
jong-k <fully.charged07@gmail.com>
2025-10-22 18:36:24 +0900

    refactor: remove unnecessary footer element from Footer component

--- src/app/_components/footer/index.tsx
+++ src/app/_components/footer/index.tsx
@@ -5,13 +5,10 @@ import FooterMenu from "./footer-menu";
 
 export default function Footer() {
   return (
-    <footer>
-      {/* footer element 없애면 쪼그라듬 */}
-      <MaxWidthWrapper>
-        <FooterMenu />
-        <Divider />
-        <BottomBanner />
-      </MaxWidthWrapper>
-    </footer>
+    <MaxWidthWrapper>
+      <FooterMenu />
+      <Divider />
+      <BottomBanner />
+    </MaxWidthWrapper>
   );
 }
