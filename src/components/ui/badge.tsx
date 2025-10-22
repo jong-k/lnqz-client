06206659e2864c0d1aaaafdbe21f35d3f2f5052c
jong-k <fully.charged07@gmail.com>
2025-10-22 11:43:31 +0900

    chore: add shadcn ui generated files

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,6 +1,6 @@
 import { cva, type VariantProps } from "class-variance-authority";
 import * as React from "react";
-import { cn } from "@/lib/utils";
+import { cn } from "@/shared/lib/utils";
 import { Slot } from "@radix-ui/react-slot";
 
 const badgeVariants = cva(
