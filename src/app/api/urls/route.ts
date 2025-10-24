c564afc6e126874fbc9f1958c97033387db76a97
jong-k <fully.charged07@gmail.com>
2025-10-24 21:18:41 +0900

    feat: add api route handler

--- src/app/api/urls/route.ts
+++ src/app/api/urls/route.ts
@@ -1,18 +1,42 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import * as z from "zod";
 
-export async function POST(request: Request) {
+const urlSchema = z.object({
+  targetUrl: z.url({
+    protocol: /^https?$/,
+  }),
+});
+
+export async function POST(request: NextRequest) {
   const { targetUrl } = await request.json();
-  if (!targetUrl) {
-    return NextResponse.json({ message: "타겟 URL이 필요합니다." }, { status: 400 });
+  const validation = urlSchema.safeParse({ targetUrl });
+  if (!validation.success) {
+    return NextResponse.json({ message: "URL이 유효하지 않습니다." }, { status: 400 });
   }
 
-  return NextResponse.json(
-    {
-      data: {
-        shortUrl: "",
-        targetUrl,
+  try {
+    const response = await fetch(`${process.env.API_URL}/urls`, {
+      body: JSON.stringify({ targetUrl }),
+      headers: {
+        "Content-Type": "application/json",
       },
-    },
-    { status: 201 }
-  );
+      method: "POST",
+    });
+    if (response.ok) {
+      const result = await response.json();
+      return NextResponse.json(
+        {
+          data: {
+            shortUrl: result.shortUrl,
+            targetUrl: result.targetUrl,
+          },
+        },
+        { status: 201 }
+      );
+    }
+  } catch (error) {
+    console.error("Error creating short URL:", error);
+    return NextResponse.json({ message: "단축 URL 생성에 실패했습니다." }, { status: 400 });
+  }
+  return NextResponse.json({ message: "단축 URL 생성에 실패했습니다." }, { status: 400 });
 }
