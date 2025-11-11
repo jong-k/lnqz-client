c930152083033308769b80e647f36dd1e9ad7232
jong-k <fully.charged07@gmail.com>
2025-11-11 16:03:49 +0900

    chore: eslint plugin update

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,39 @@
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 import { importX } from "eslint-plugin-import-x";
-import pluginPerfectionist from "eslint-plugin-perfectionist";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import { configs as sonarConfigs } from "eslint-plugin-sonarjs";
 import pluginUnicorn from "eslint-plugin-unicorn";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import { configs as tsConfigs } from "typescript-eslint";
 import js from "@eslint/js";
-import pluginNext from "@next/eslint-plugin-next";
 
 export default defineConfig([
-  { ignores: [".next/**", "./src/shared/shadcn-ui/**"] },
-  {
-    extends: ["js/recommended"],
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    languageOptions: { globals: globals.browser },
-    plugins: { js },
-  },
-
-  importX.flatConfigs.recommended,
+  importX.flatConfigs.react,
   importX.flatConfigs.typescript,
-  ...tsConfigs.recommended,
-  pluginNext.flatConfig.coreWebVitals,
-  pluginUnicorn.configs.recommended,
-  pluginPerfectionist.configs["recommended-natural"],
+  js.configs.recommended,
   {
-    files: ["**/*.{js,mjs,cjs,ts,tsx,mts,cts}"],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    ignores: ["src/middleware.ts", "src/app/**/route.ts"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "no-console": "warn",
+    },
+  },
+  { files: ["src/middleware.ts", "src/app/**/route.ts"], languageOptions: { globals: globals.node } },
+  ...tsConfigs.recommended,
+  ...nextVitals,
+  ...nextTs,
+  pluginUnicorn.configs.recommended,
+  sonarConfigs.recommended,
+  {
     rules: {
       "@typescript-eslint/triple-slash-reference": "off",
       "import-x/no-named-as-default-member": "off",
-      "perfectionist/sort-imports": "off",
       "unicorn/prevent-abbreviations": "off",
     },
   },
-  sonarConfigs.recommended,
   eslintPluginPrettierRecommended,
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
