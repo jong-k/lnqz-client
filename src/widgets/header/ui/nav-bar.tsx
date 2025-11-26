5a2b2d817d593f10b3a44fcf47a2e46eaa2e1c3d
jong-k <fully.charged07@gmail.com>
2025-11-26 12:06:19 +0900

    feat: NavBar 컴포넌트에 i18n 적용

--- src/widgets/header/ui/nav-bar.tsx
+++ src/widgets/header/ui/nav-bar.tsx
@@ -1,19 +1,25 @@
 "use client";
 
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 import { toast } from "sonner";
 import { Button } from "@/shared/shadcn-ui/components/ui/button";
 import { Logo } from "@/shared/ui";
+import SelectLocale from "./select-locale";
 
 export default function NavBar() {
+  const t = useTranslations("ui.header");
   return (
     <nav className="flex items-center justify-between">
       <Link href="/">
         <Logo alt="Link Squeeze" height={100} priority sizes="100px" width={100} />
       </Link>
-      <Button className="cursor-pointer" onClick={() => toast.info("🚧 기능 준비중 🚧")} variant="ghost">
-        공지사항
-      </Button>
+      <div className="flex items-center gap-2">
+        <SelectLocale />
+        <Button className="cursor-pointer" onClick={() => toast.info("🚧 기능 준비중 🚧")} variant="ghost">
+          {t("announcement")}
+        </Button>
+      </div>
     </nav>
   );
 }
