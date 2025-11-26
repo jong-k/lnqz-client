48b746d3e7b9d6999b093042b1f88ac013c65a4c
jong-k <fully.charged07@gmail.com>
2025-11-26 10:50:23 +0900

    feat: 카피라이트 기호를 HTML 엔터티로 변경

--- src/widgets/footer/ui/bottom-banner.tsx
+++ src/widgets/footer/ui/bottom-banner.tsx
@@ -13,7 +13,7 @@ export default function BottomBanner() {
         </Link>
       </div>
       <div>
-        © 2025 {t("metaData.title")} | {t("page.home.heading")}
+        &copy; 2025 {t("metaData.title")} | {t("page.home.heading")}
       </div>
       <div>
         <div>Made by</div>
