158f98dfc02858b017bf319b12b9a967b95f087d
jong-k <fully.charged07@gmail.com>
2025-11-26 21:54:38 +0900

    refactor: LocaleSwitcher 컴포넌트 shadcn ui 적용

--- src/features/switch-locale/ui/locale-switcher.tsx
+++ src/features/switch-locale/ui/locale-switcher.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import type { AppLocale } from "@/shared/i18n/lib";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/shared/shadcn-ui/components/ui/select";
 import { changeLocale } from "../api";
 import { LOCALE_OPTIONS } from "../lib";
 
@@ -8,18 +9,21 @@ interface LocaleSwitcherProps {
   currentLocale: AppLocale;
 }
 
-const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-  changeLocale(e.target.value as AppLocale);
-};
-
 export function LocaleSwitcher({ currentLocale }: LocaleSwitcherProps) {
+  const currentLocaleLabel = LOCALE_OPTIONS.find(option => option.value === currentLocale)?.label ?? "한국어";
+
   return (
-    <select className="text-sm font-medium" defaultValue={currentLocale} onChange={handleChange}>
-      {LOCALE_OPTIONS.map(({ label, value }) => (
-        <option key={value} value={value}>
-          {label}
-        </option>
-      ))}
-    </select>
+    <Select defaultValue={currentLocale} onValueChange={value => changeLocale(value as AppLocale)}>
+      <SelectTrigger className="min-w-[100px]">
+        <SelectValue placeholder={currentLocaleLabel} />
+      </SelectTrigger>
+      <SelectContent className="min-w-[100px]">
+        {LOCALE_OPTIONS.map(({ label, value }) => (
+          <SelectItem key={value} value={value}>
+            {label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
   );
 }
