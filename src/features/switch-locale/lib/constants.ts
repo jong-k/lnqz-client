e7c77538b4478d7c55c114f31d7713f8834000ee
jong-k <fully.charged07@gmail.com>
2025-11-26 22:27:16 +0900

    refactor: LOCALE_OPTIONS 동적으로 만들어지게 변경

--- src/features/switch-locale/lib/constants.ts
+++ src/features/switch-locale/lib/constants.ts
@@ -1,4 +1,11 @@
-export const LOCALE_OPTIONS = [
-  { label: "한국어", value: "ko" },
-  { label: "English", value: "en" },
-] as const;
+import { APP_LOCALES } from "@/shared/i18n/lib";
+
+const LOCALE_LABEL_MAP = {
+  ko: "한국어",
+  en: "English",
+} as const;
+
+export const LOCALE_OPTIONS = APP_LOCALES.map(locale => ({
+  label: LOCALE_LABEL_MAP[locale],
+  value: locale,
+}));
