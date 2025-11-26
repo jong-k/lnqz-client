799cf05174b1affcebf553e82958287404b1ef21
jong-k <fully.charged07@gmail.com>
2025-11-26 21:55:00 +0900

    chore: 불필요 path 프로퍼티 제거

--- src/features/switch-locale/api/change-locale.ts
+++ src/features/switch-locale/api/change-locale.ts
@@ -5,10 +5,9 @@ import type { AppLocale } from "@/shared/i18n/lib";
 
 export const changeLocale = async (locale: AppLocale) => {
   const cookieStore = await cookies();
-  cookieStore.set("locale", locale, { 
-    httpOnly: true, 
-    secure: process.env.NODE_ENV === "production", 
-    sameSite: "lax", 
-    path: "/" 
+  cookieStore.set("locale", locale, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
   });
 };
