a43d2dc36fa1e37a596486c0c8dc7e5f32431bcc
jong-k <fully.charged07@gmail.com>
2025-11-26 15:58:34 +0900

    refactor: TopBanner 컴포넌트 단순화하여 AppHeader 컴포넌트를 서버 컴포넌트로 변경

--- src/widgets/header/ui/app-header.tsx
+++ src/widgets/header/ui/app-header.tsx
@@ -1,25 +1,11 @@
-"use client";
-
-import { useTranslations } from "next-intl";
-import { usePathname } from "next/navigation";
 import { ResponsiveWrapper } from "@/shared/ui";
 import NavBar from "./nav-bar";
 import TopBanner from "./top-banner";
 
 export function AppHeader() {
-  const pathname = usePathname();
-  const showTopBanner = pathname === "/";
-  const t = useTranslations();
-
   return (
     <header className="relative z-10 w-full shadow-sm">
-      {showTopBanner && (
-        <TopBanner
-          className="hidden sm:flex"
-          text={`🎉 ${t("metaData.title")} ${t("ui.topBanner.serviceLaunch")} 🎉`}
-          url="/"
-        />
-      )}
+      <TopBanner />
       <ResponsiveWrapper>
         <NavBar />
       </ResponsiveWrapper>
