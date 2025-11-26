f2f533176ce1a5a09dfc1f5b0cbbaffd0a4c4746
jong-k <fully.charged07@gmail.com>
2025-11-26 12:12:03 +0900

    feat: ShortenerContainer 컴포넌트에 i18n 적용

--- src/features/shorten-url/ui/shortener-container.tsx
+++ src/features/shorten-url/ui/shortener-container.tsx
@@ -1,10 +1,13 @@
+import { getTranslations } from "next-intl/server";
 import ShortenerForm from "./shortener-form";
 
-export function ShortenerContainer() {
+export async function ShortenerContainer() {
+  const t = await getTranslations();
+
   return (
     <div>
-      <h3 className="text-center text-3xl font-semibold">링크를 줄이세요, 당신의 시간은 소중하니까</h3>
-      <p className="my-4 text-center text-xl">긴 URL을 짧게 줄일 수 있습니다</p>
+      <h3 className="text-center text-3xl font-semibold">{t("metaData.slogan")}</h3>
+      <p className="my-4 text-center text-xl">{t("page.home.subHeading")}</p>
       <div className="mx-auto max-w-2xl rounded-md bg-white p-8 shadow-md">
         <ShortenerForm />
       </div>
