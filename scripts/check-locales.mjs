40a01986470273afdc6f9683ff4a4c2879490e63
jong-k <fully.charged07@gmail.com>
2025-11-26 23:28:55 +0900

    feat: locales check 스크립트 추가

---
+++ scripts/check-locales.mjs
@@ -0,0 +1,61 @@
+/* eslint-disable no-console */
+import { readFile } from "node:fs/promises";
+
+const files = ["src/shared/i18n/config/messages/ko.json", "src/shared/i18n/config/messages/en.json"];
+
+const loadJson = async path => {
+  try {
+    const jsonContent = await readFile(path, "utf8");
+    return JSON.parse(jsonContent);
+  } catch (error) {
+    console.error(`Error reading or parsing JSON file at ${path}:`, error);
+  }
+};
+
+const getAllkeys = (obj, parentKey = "") => {
+  let keys = [];
+  for (const key in obj) {
+    const newKey = parentKey ? `${parentKey}.${key}` : key;
+    if (typeof obj[key] === "object" && obj[key] !== null) {
+      keys = [...keys, ...getAllkeys(obj[key], newKey)];
+    } else {
+      keys.push(newKey);
+    }
+  }
+  return keys;
+};
+
+const checkKeys = async () => {
+  let hasError = false;
+  const koJson = await loadJson(files[0]);
+  const enJson = await loadJson(files[1]);
+
+  const koKeys = getAllkeys(koJson);
+  const enKeys = getAllkeys(enJson);
+
+  const missingInKo = enKeys.filter(key => !koKeys.includes(key));
+  const missingInEn = koKeys.filter(key => !enKeys.includes(key));
+  console.log("Checking for missing keys between ko.json and en.json...");
+
+  if (missingInKo.length > 0) {
+    hasError = true;
+    console.error("Missing keys in ko.json:", missingInKo);
+  } else {
+    console.log("No missing keys in ko.json");
+  }
+
+  if (missingInEn.length > 0) {
+    hasError = true;
+    console.error("Missing keys in en.json:", missingInEn);
+  } else {
+    console.log("No missing keys in en.json");
+  }
+  if (hasError) globalThis.process.exit(1);
+};
+
+try {
+  await checkKeys();
+  console.log("Locales check completed successfully");
+} catch (error) {
+  console.error("Error during locales check:", error);
+}
