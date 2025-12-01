110703a28024bbfa6bfba581896ba2cada549c91
jong-k <fully.charged07@gmail.com>
2025-12-01 15:59:15 +0900

    feat: onClick props 추가

--- src/shared/ui/icon-button.tsx
+++ src/shared/ui/icon-button.tsx
@@ -3,15 +3,18 @@ import { cn } from "../shadcn-ui/lib";
 interface IconButtonProps {
   children?: React.ReactNode;
   className?: string;
+  onClick?: () => void;
 }
 
-export function IconButton({ children, className }: IconButtonProps) {
+export function IconButton({ children, className, onClick }: IconButtonProps) {
   return (
     <button
       className={cn(
         "cursor-pointer text-muted-foreground hover:text-foreground focus:outline-none focus-visible:ring-2 focus-visible:ring-sky-500",
         className
       )}
+      onClick={onClick}
+      type="button"
     >
       {children}
     </button>
